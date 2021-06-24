e596af9fd3bd71d22c16aba205a5f1327637ed0f
fajar13k <fajar.fj13@gmail.com>
2021-06-24 23:25:51 +0700

    Add nav drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,9 @@
 import 'regenerator-runtime';
+import '../styles/style.css';
+import App from './views/app';
 
-console.log('Hello Coders!');
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
