6683110f841ef9e2c3085dfd2ad07f85d10040f3
fajar13k <fajar.fj13@gmail.com>
2021-06-27 17:20:13 +0700

    Change city to have self div

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -9,10 +9,14 @@ const createRestaurantItemTemplate = (restaurant) => `
     <img src="${restaurant.id ? CONFIG.BASE_IMAGE_URL_SMALL + restaurant.pictureId : 'https://picsum.photos/id/1060/403/270?grayscale'}" alt="Restoran ${restaurant.name} di ${restaurant.city}">
   </picture>
   <div class="grid-restoran__text__overlay">
-    <h1>⭐️ ${restaurant.rating}/5</h1>
+    <p>⭐️ ${restaurant.rating}/5</p>
   </div>
   <div class="grid-restoran__content">
-    <h2>${restaurant.name} ${restaurant.city}</h2>
+    <div class="grid-restoran__lokasi">
+      <i class="fa fa-map-marker-alt"></i>
+      <p>${restaurant.city}</p>
+    </div>
+    <h1>${restaurant.name}</h1>
     <p>${truncateString(restaurant.description, 250)}</p>
     <button class="btn button__medium button__main">Cek Restoran</button>
   </div>
