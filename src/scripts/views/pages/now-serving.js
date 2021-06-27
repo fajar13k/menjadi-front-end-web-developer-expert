6fb1d88d7e295519fc6af659e32ea3147fef1f41
fajar13k <fajar.fj13@gmail.com>
2021-06-27 11:19:41 +0700

    Overhaul card design

--- src/scripts/views/pages/now-serving.js
+++ src/scripts/views/pages/now-serving.js
@@ -1,5 +1,5 @@
 import RestaurantSource from '../../data/restaurant-source';
-import createRestaurantDetailTemplate from '../templates/template-creator';
+import createRestaurantItemTemplate from '../templates/template-creator';
 
 const NowServing = {
   async render() {
@@ -7,11 +7,11 @@ const NowServing = {
     <div class="content">
       <section class="content">
         <div class="whats__up">
-            <h1 class="whats__up__tagline">いらっしゃいませ！</h1>
+            <h1 class="whats__up__tagline">Selamat Datang！</h1>
             <h2 class="whats__up__headline">Mau makan apa <span class="highlight">sekarang?</span></h2>
         </div>
       </section>
-      <div class="posts" id="list__restoran">
+      <div class="grid-restoran" id="list__restoran">
           <div id="card__restoran"></div>
       </div>
     </div>
@@ -22,7 +22,7 @@ const NowServing = {
     const resto = await RestaurantSource.nowServingRestaurant();
     const restoContainer = document.querySelector('#card__restoran');
     resto.restaurants.forEach((restaurant) => {
-      restoContainer.innerHTML += createRestaurantDetailTemplate(restaurant);
+      restoContainer.innerHTML += createRestaurantItemTemplate(restaurant);
     });
   },
 };
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,25 +1,19 @@
 import CONFIG from '../../globals/config';
 import truncateString from '../../utils/truncate-string';
 
-const createRestaurantDetailTemplate = (restaurant) => `
-  <article class="post-item" tabindex="0">
-  <div class="box">
-    <picture class="post-item__thumbnail">
-      <source media="(min-width:1024px)" srcset="${restaurant.id ? CONFIG.BASE_IMAGE_URL_LARGE + restaurant.pictureId : 'https://picsum.photos/id/1060/1210/810?grayscale'}">
-      <source media="(min-width:768px)" srcset="${restaurant.id ? CONFIG.BASE_IMAGE_URL_MEDIUM + restaurant.pictureId : 'https://picsum.photos/id/1060/807/540?grayscale'}">
-      <img src="${restaurant.id ? CONFIG.BASE_IMAGE_URL_SMALL + restaurant.pictureId : 'https://picsum.photos/id/1060/403/270?grayscale'}" alt="Restoran ${restaurant.name} di ${restaurant.city}">
-    </picture>
-    <div class="post-item__overlay">
-      <h1>${restaurant.city}</h1>
-    </div>
-  </div>
-  <div class="post-item__content">
-      <p class="post-item__rating">Rating: <span class="post-item__rating__score">${restaurant.rating}/5</span>
-      </p>
-      <h1 class="post-item__title" tabindex="-1"><a href="#" tabindex="-1">${restaurant.name}</a></h1>
-      <p class="post-item__description">${truncateString(restaurant.description, 250)}</p>
+const createRestaurantItemTemplate = (restaurant) => `
+  <article tabindex="0">
+  <picture>
+    <source media="(min-width:1024px)" srcset="${restaurant.id ? CONFIG.BASE_IMAGE_URL_LARGE + restaurant.pictureId : 'https://picsum.photos/id/1060/1210/810?grayscale'}">
+    <source media="(min-width:768px)" srcset="${restaurant.id ? CONFIG.BASE_IMAGE_URL_MEDIUM + restaurant.pictureId : 'https://picsum.photos/id/1060/807/540?grayscale'}">
+    <img src="${restaurant.id ? CONFIG.BASE_IMAGE_URL_SMALL + restaurant.pictureId : 'https://picsum.photos/id/1060/403/270?grayscale'}" alt="Restoran ${restaurant.name} di ${restaurant.city}">
+  </picture>
+  <div class="grid-restoran__content">
+    <h1>${restaurant.name}</h3>
+    <p>${truncateString(restaurant.description, 250)}</p>
+    <button class="btn button__medium button__main">Cek Restoran</button>
   </div>
   </article>
 `;
 
-export default createRestaurantDetailTemplate;
+export default createRestaurantItemTemplate;
