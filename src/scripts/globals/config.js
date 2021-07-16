645c58297de2f57ccc64ee0a1a4a47d83c9ab4b1
fajar13k <fajar.fj13@gmail.com>
2021-07-16 22:53:20 +0700

    Change cache key config to static to improve performance

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   BASE_IMAGE_URL_SMALL: 'https://restaurant-api.dicoding.dev/images/small/',
   BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
   BASE_IMAGE_URL_LARGE: 'https://restaurant-api.dicoding.dev/images/large/',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'Restopedia-V1',
   DATABASE_NAME: 'restopedia-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurant',
